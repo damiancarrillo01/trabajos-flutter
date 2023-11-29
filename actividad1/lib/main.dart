e4f1a1a3c4b1cad9d9a6d5af45f3136d2bca87ab
damiancarrillo01 <damian.15carrillod@gmail.com>
2023-11-29 16:34:34 -0300

    flutter

---
+++ actividad1/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi Aplicación'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print('¡Hola Mundo!');
+          },
+          child: Text('Presiona para un saludo'),
+        ),
+      ),
+    );
+  }
+}
